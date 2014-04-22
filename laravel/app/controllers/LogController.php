801abf7000d5f0fec088b7cd174d58d7b229ef74
eblipsky <eblipsky@gmail.com>
2014-04-22 13:50:10 -0400

    looks better without the hr tag

--- laravel/app/controllers/LogController.php
+++ laravel/app/controllers/LogController.php
@@ -10,7 +10,7 @@ class LogController extends \BaseController {
         $url = Settings::COUCHDB_HOST."/".Settings::COUCHDB_DB."/".$logid."/logfile";
 
         $str = nl2br(file_get_contents($url));
-        $str = str_replace("<br />", "<hr>", $str);                        
+        //$str = str_replace("<br />", "<hr>", $str);                        
         $this->data['file'] = $str;                        
 
         return View::make('hpc/log',$this->data); 
